13c3ffb96de733483a1fa4957e36bbec7bad0ada
Nathanael Olsson <no222hj@student.lnu.se>
2023-09-18 13:07:28 +0200

    Remove old clutter from building.

--- src/js/pie-chart/pie-chart.js
+++ src/js/pie-chart/pie-chart.js
@@ -16,29 +16,6 @@ export class PieChart {
     this.chartData = data
   }
 
-  generateChart() {
-    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
-    svg.setAttribute("width", "200")
-    svg.setAttribute("height", "200")
-    let pieCircumference = 2 * Math.PI * 100
-    const pieSliceOne = document.createElementNS("http://www.w3.org/2000/svg", "path")
-    pieSliceOne.setAttribute("d", "M100,100 L200,100 A100,100 0 0,1 100,200 Z")
-    pieSliceOne.setAttribute("fill", "red")
-    const pieSliceTwo = document.createElementNS("http://www.w3.org/2000/svg", "path")
-    pieSliceTwo.setAttribute("d", "M100,100 L100,200 A100,100 0 0,1 0,100 Z")
-    pieSliceTwo.setAttribute("fill", "green")
-    const pieSliceThree = document.createElementNS("http://www.w3.org/2000/svg", "path")	
-    pieSliceThree.setAttribute("d", "M100,100 L0,100 A100,100 0 0,1 100,0 Z")
-    pieSliceThree.setAttribute("fill", "blue")
-    const pieSliceFour = document.createElementNS("http://www.w3.org/2000/svg", "path")
-    pieSliceFour.setAttribute("d", "M100,100 L100,0 A100,100 0 0,1 200,100 Z")
-    svg.appendChild(pieSliceOne)
-    svg.appendChild(pieSliceTwo)
-    svg.appendChild(pieSliceThree)
-    svg.appendChild(pieSliceFour)
-    return svg
-  }
-
   createChart() { 
     const data = this.chartData
     console.log(data)
