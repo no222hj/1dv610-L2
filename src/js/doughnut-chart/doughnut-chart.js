f5e9269129d2d5998e95246c09eaf9a96009cbf1
Nathanael Olsson <no222hj@student.lnu.se>
2023-09-24 19:53:56 +0200

    Edit start angle.

--- src/js/doughnut-chart/doughnut-chart.js
+++ src/js/doughnut-chart/doughnut-chart.js
@@ -16,21 +16,22 @@ export class DoughnutChart {
       this.chartData = data
       this.chartOptions = options //use setter in future.
       this.chartWidth = this.chartOptions.size.width
-      this.radian = this.chartWidth / 2 * 0.75
+      this.doughnutWidth = this.chartWidth * 0.75
+      this.radian = this.doughnutWidth / 2
       this.innerRadius = 0.5
     }
   
   
     createDoughnutChart() { 
       const data = this.chartData
-      let currentAngleInRadians = 360
+      let currentAngleInRadians = this.#convertToRadians(270)
       const doughnutGroup = document.createElementNS("http://www.w3.org/2000/svg", "g")
       for (let index = 0; index < data.length; index++) {
-        const doughnutPart = this.generatePart(data[index], currentAngleInRadians)
+        const doughnutPart = this.#generatePart(data[index], currentAngleInRadians)
         const title = document.createElementNS("http://www.w3.org/2000/svg", "title")
         title.textContent = `${data[index].desc}: ${data[index].amount}`
         doughnutPart.appendChild(title)
-        currentAngleInRadians += this.convertToRadians(data[index].percent * 360)
+        currentAngleInRadians += this.#convertToRadians(data[index].percent * 360)
         doughnutGroup.appendChild(doughnutPart)
       }
       console.log(doughnutGroup)
@@ -44,23 +45,25 @@ export class DoughnutChart {
     }
   
   
-    generatePart(data, startAngleInRadians) {
+    #generatePart(data, startAngleInRadians) {
       const doughnutPart = document.createElementNS("http://www.w3.org/2000/svg", "path")
       const startOuterX = this.chartWidth / 2 + Math.cos(startAngleInRadians) * this.radian
       const startOuterY = this.chartWidth / 2 + Math.sin(startAngleInRadians) * this.radian
-      const endOuterX = this.chartWidth / 2 + Math.cos(startAngleInRadians + this.convertToRadians(data.percent * 360)) * this.radian
-      const endOuterY = this.chartWidth / 2 + Math.sin(startAngleInRadians + this.convertToRadians(data.percent * 360)) * this.radian
+      const endOuterX = this.chartWidth / 2 + Math.cos(startAngleInRadians + this.#convertToRadians(data.percent * 360)) * this.radian
+      const endOuterY = this.chartWidth / 2 + Math.sin(startAngleInRadians + this.#convertToRadians(data.percent * 360)) * this.radian
 
       const startInnerX = this.chartWidth / 2 + Math.cos(startAngleInRadians) * this.radian * this.innerRadius
       const startInnerY = this.chartWidth / 2 + Math.sin(startAngleInRadians) * this.radian * this.innerRadius
-      const endInnerX = this.chartWidth / 2 + Math.cos(startAngleInRadians + this.convertToRadians(data.percent * 360)) * this.radian * this.innerRadius
-      const endInnerY = this.chartWidth / 2 + Math.sin(startAngleInRadians + this.convertToRadians(data.percent * 360)) * this.radian * this.innerRadius    
+      const endInnerX = this.chartWidth / 2 + Math.cos(startAngleInRadians + this.#convertToRadians(data.percent * 360)) * this.radian * this.innerRadius
+      const endInnerY = this.chartWidth / 2 + Math.sin(startAngleInRadians + this.#convertToRadians(data.percent * 360)) * this.radian * this.innerRadius    
+
+       console.log(startOuterX, startInnerX)
   
-      const d = `M${startInnerX},${startInnerY}` + 
-                `L${startOuterX},${startOuterY}` + 
-                `A${this.radian},${this.radian}, 0 0,1 ${endOuterX},${endOuterY}` +
-                `L${endInnerX},${endInnerY}` +
-                `A${this.radian * this.innerRadius},${this.radian * this.innerRadius}, 0 0,0 ${startInnerX},${startInnerY} Z}`
+      const d = `M ${startInnerX} ${startInnerY} ` + 
+                `L ${startOuterX} ${startOuterY} ` + 
+                `A ${this.radian} ${this.radian} 0 0 1 ${endOuterX} ${endOuterY} ` +
+                `L ${endInnerX} ${endInnerY} ` +
+                `A ${this.radian * this.innerRadius} ${this.radian * this.innerRadius} 0 0 0 ${startInnerX} ${startInnerY} Z}`
 
       doughnutPart.setAttribute("d", `${d}`)
       doughnutPart.setAttribute("fill", data.color)
@@ -69,7 +72,7 @@ export class DoughnutChart {
       return doughnutPart
     }
   
-    convertToRadians(degrees) {
+    #convertToRadians(degrees) {
       return degrees * Math.PI / 180
     }
   
--- src/js/pie-chart/pie-chart.js
+++ src/js/pie-chart/pie-chart.js
@@ -7,32 +7,35 @@ import { Effects } from "../effects/effects.js"
  * @verison 1.0.0
  */
 
-/**
- * Creates a pie chart svg
- */
-
 export class PieChart {
-
 
   constructor(data, options) {
     this.chartData = data
     this.chartOptions = options //use setter in future.
     this.chartWidth = this.chartOptions.size.width
-    this.radian = this.chartWidth / 2 * 0.75
+    this.pieWidth = this.chartWidth * 0.75
+    this.radian = this.pieWidth / 2
   }
 
-
+  /**
+   * Creates a pie chart
+   * 
+   * @returns {object} pieGroup
+   * 
+   * @memberof PieChart
+   */
   createPieChart() { 
     const data = this.chartData
-    let currentAngleInRadians = 360
+    let currentAngleInRadians = this.#convertToRadians(270)
+    console.log(currentAngleInRadians)
     const pieGroup = document.createElementNS("http://www.w3.org/2000/svg", "g")
     for (let index = 0; index < data.length; index++) {
-      const pieSlice = this.generateSlice(data[index], currentAngleInRadians)
+      const pieSlice = this.#generateSlice(data[index], currentAngleInRadians)
       pieSlice.setAttribute("stroke", "white")
       const title = document.createElementNS("http://www.w3.org/2000/svg", "title")
       title.textContent = `${data[index].desc}: ${data[index].amount} \n ${data[index].percent * 100}%`
       pieSlice.appendChild(title)
-      currentAngleInRadians += this.convertToRadians(data[index].percent * 360)
+      currentAngleInRadians += this.#convertToRadians(data[index].percent * 360)
       const effects = new Effects()
       effects.addTooltip(pieSlice)
       pieGroup.appendChild(pieSlice)
@@ -47,21 +50,32 @@ export class PieChart {
   }
 
 
-  generateSlice(data, startAngleInRadians) {
+  /**
+   * Generates a pie slice
+   * 
+   * @param {object} data - The data for the pie slice.
+   * @param {number} startAngleInRadians - The start angle of the pie slice in radians.
+   * @returns {object} pieSlice
+   * 
+   * @memberof PieChart
+   * @private
+   */
+  #generateSlice(data, startAngleInRadians) {
     const pieSlice = document.createElementNS("http://www.w3.org/2000/svg", "path")
     const startX = this.chartWidth / 2 + Math.cos(startAngleInRadians) * this.radian
     const startY = this.chartWidth / 2 + Math.sin(startAngleInRadians) * this.radian
-    const endX = this.chartWidth / 2 + Math.cos(startAngleInRadians + this.convertToRadians(data.percent * 360)) * this.radian
-    const endY = this.chartWidth / 2 + Math.sin(startAngleInRadians + this.convertToRadians(data.percent * 360)) * this.radian
+    const endX = this.chartWidth / 2 + Math.cos(startAngleInRadians + this.#convertToRadians(data.percent * 360)) * this.radian
+    const endY = this.chartWidth / 2 + Math.sin(startAngleInRadians + this.#convertToRadians(data.percent * 360)) * this.radian
 
 
     pieSlice.setAttribute("d", `M${this.chartWidth / 2},${this.chartWidth / 2} L${startX},${startY} A${this.radian},${this.radian}, 0 0,1 ${endX},${endY} Z`)
+    console.log(this.radian)
     pieSlice.setAttribute("fill", data.color)
 
     return pieSlice
   }
 
-  convertToRadians(degrees) {
+  #convertToRadians(degrees) {
     return degrees * Math.PI / 180
   }
 
